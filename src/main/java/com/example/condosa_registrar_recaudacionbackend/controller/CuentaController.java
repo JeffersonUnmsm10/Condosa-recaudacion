d9ab4098a483d8799c6d2496a64a92874b408cf1
JeffersonUnmsm10 <143053248+JeffersonUnmsm10@users.noreply.github.com>
2023-11-21 01:59:02 -0500

    Solicitudes GET

--- src/main/java/com/example/condosa_registrar_recaudacionbackend/controller/CuentaController.java
+++ src/main/java/com/example/condosa_registrar_recaudacionbackend/controller/CuentaController.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+// Controlador bien estructurado que maneja las solicitudes GET 
+// para recuperar información de cuentas bancarias basadas en números de documento.
+
 @RestController
 @RequestMapping("/api/v1/cuenta")
 @CrossOrigin("*")
